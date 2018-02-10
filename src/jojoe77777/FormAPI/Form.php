c29e225b989f7125afb4ce56ce0dc59f04e7c64f
jojoe77777 <jojoe77777@users.noreply.github.com>
2018-02-10 21:39:00 +1100

    Implemented labels for simple forms, changed null values for custom forms (#20)
    Implemented labels for simple forms, changed null values for custom forms [BC]

--- src/jojoe77777/FormAPI/Form.php
+++ src/jojoe77777/FormAPI/Form.php
@@ -9,50 +9,50 @@ use pocketmine\Player;
 
 abstract class Form {
 
-	/** @var int */
-	public $id;
-	/** @var array */
-	private $data = [];
-	/** @var string */
-	public $playerName;
-	/** @var callable */
-	private $callable;
+    /** @var int */
+    public $id;
+    /** @var array */
+    private $data = [];
+    /** @var string */
+    public $playerName;
+    /** @var callable */
+    private $callable;
 
-	/**
-	 * @param int $id
-	 * @param callable $callable
-	 */
-	public function __construct(int $id, ?callable $callable) {
-		$this->id = $id;
-		$this->callable = $callable;
-	}
+    /**
+     * @param int $id
+     * @param callable $callable
+     */
+    public function __construct(int $id, ?callable $callable) {
+        $this->id = $id;
+        $this->callable = $callable;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getId() : int {
-		return $this->id;
-	}
+    /**
+     * @return int
+     */
+    public function getId() : int {
+        return $this->id;
+    }
 
-	/**
-	 * @param Player $player
-	 */
-	public function sendToPlayer(Player $player) : void {
-		$pk = new ModalFormRequestPacket();
-		$pk->formId = $this->id;
-		$pk->formData = json_encode($this->data);
-		$player->dataPacket($pk);
-		$this->playerName = $player->getName();
-	}
+    /**
+     * @param Player $player
+     */
+    public function sendToPlayer(Player $player) : void {
+        $pk = new ModalFormRequestPacket();
+        $pk->formId = $this->id;
+        $pk->formData = json_encode($this->data);
+        $player->dataPacket($pk);
+        $this->playerName = $player->getName();
+    }
 
-	public function isRecipient(Player $player) : bool {
-		return $player->getName() === $this->playerName;
-	}
+    public function isRecipient(Player $player) : bool {
+        return $player->getName() === $this->playerName;
+    }
 
-	public function getCallable() : ?callable {
-		return $this->callable;
-	}
+    public function getCallable() : ?callable {
+        return $this->callable;
+    }
 
-	public function processData(&$data) : void {
-	}
+    public function processData(&$data) : void {
+    }
 }
