06860bc5161a66e207ad44879dbe1d667eb68af2
DragonfireHD <50929188+DragonfireHD@users.noreply.github.com>
2021-12-19 21:09:43 +0100

    API 4

--- src/jojoe77777/FormAPI/Form.php
+++ src/jojoe77777/FormAPI/Form.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 namespace jojoe77777\FormAPI;
 
 use pocketmine\form\Form as IForm;
-use pocketmine\Player;
+use pocketmine\player\Player;
 
 abstract class Form implements IForm{
 
