7fd43d84932fe6a182db1758ede684dfafe3760b
Dylan K. Taylor <dktapps@pmmp.io>
2022-01-12 19:03:13 +0000

    PM4

--- src/jojoe77777/FormAPI/FormAPI.php
+++ src/jojoe77777/FormAPI/FormAPI.php
@@ -8,33 +8,4 @@ use pocketmine\plugin\PluginBase;
 
 class FormAPI extends PluginBase{
 
-    /**
-     * @deprecated
-     *
-     * @param callable|null $function
-     * @return CustomForm
-     */
-    public function createCustomForm(?callable $function = null) : CustomForm {
-        return new CustomForm($function);
-    }
-
-    /**
-     * @deprecated
-     *
-     * @param callable|null $function
-     * @return SimpleForm
-     */
-    public function createSimpleForm(?callable $function = null) : SimpleForm {
-        return new SimpleForm($function);
-    }
-
-    /**
-     * @deprecated
-     *
-     * @param callable|null $function
-     * @return ModalForm
-     */
-    public function createModalForm(?callable $function = null) : ModalForm {
-        return new ModalForm($function);
-    }
 }
