691d98898a61ba88928e415af856098a7b015399
Dylan K. Taylor <dktapps@pmmp.io>
2022-01-12 19:13:53 +0000

    Fixed SimpleForm closing selecting the first option

--- src/jojoe77777/FormAPI/SimpleForm.php
+++ src/jojoe77777/FormAPI/SimpleForm.php
@@ -28,14 +28,16 @@ class SimpleForm extends Form {
     }
 
     public function processData(&$data) : void {
-        if($data !== null && !is_int($data)) {
-            throw new FormValidationException("Expected an integer response, got " . gettype($data));
+        if($data !== null){
+            if(!is_int($data)) {
+                throw new FormValidationException("Expected an integer response, got " . gettype($data));
+            }
+            $count = count($this->data["buttons"]);
+            if($data >= $count || $data < 0) {
+                throw new FormValidationException("Button $data does not exist");
+            }
+            $data = $this->labelMap[$data] ?? null;
         }
-        $count = count($this->data["buttons"]);
-        if($data >= $count || $data < 0) {
-            throw new FormValidationException("Button $data does not exist");
-        }
-        $data = $this->labelMap[$data] ?? null;
     }
 
     /**
